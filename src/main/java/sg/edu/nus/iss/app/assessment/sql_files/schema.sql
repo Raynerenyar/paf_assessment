4a17f732bf6fadb638e4b7f0254e6b870e2a0843
Raynerenyar <105347568+Raynerenyar@users.noreply.github.com>
2023-03-03 16:42:27 +0800

    submit

--- src/main/java/sg/edu/nus/iss/app/assessment/sql_files/schema.sql
+++ src/main/java/sg/edu/nus/iss/app/assessment/sql_files/schema.sql
@@ -1,20 +1,13 @@
--- create database acme_bank;
+create database if not exists acme_bank;
 
--- use acme_bank;
+use acme_bank;
 
--- create table if not exists accounts (
--- 	`account_id` varchar(10) not null,
---     `name` char(15) not null,
---     `balance` decimal(10,2) not null
--- 		PRIMARY KEY (`account_id`);
--- );
-
--- V9L3Jd1BBI, fred, 100.00
--- fhRq46Y6vB, barney, 300.00
--- uFSFRqUpJy, wilma, 1000.00
--- ckTV56axff, betty, 1000.00
--- Qgcnwbshbh, pebbles, 50.00
--- if9l185l18, bambam, 50.00
+ create table if not exists accounts (
+ 	`account_id` varchar(10) not null,
+     `name` char(15) not null,
+     `balance` decimal(10,2) not null,
+ 		PRIMARY KEY (`account_id`)
+);
 
 insert into accounts (`account_id`, `name`, `balance`) values ('V9L3Jd1BBI', 'fred', 100.00);
 insert into accounts (`account_id`, `name`, `balance`) values ('fhRq46Y6vB', 'barney', 300.00);
@@ -22,5 +15,3 @@ insert into accounts (`account_id`, `name`, `balance`) values ('uFSFRqUpJy', 'wi
 insert into accounts (`account_id`, `name`, `balance`) values ('ckTV56axff', 'betty', 1000.00);
 insert into accounts (`account_id`, `name`, `balance`) values ('Qgcnwbshbh', 'pebbles', 50.00);
 insert into accounts (`account_id`, `name`, `balance`) values ('if9l185l18', 'bambam', 50.00);
- 
-
